917d2fb2ef98a1cb529937e9694f83f43869a692
FreshSeeker <i.zhoukang@foxmail.com>
2018-10-12 23:23:51 +0800

    1.更换api key； 2.保存退出前的城市名称；

--- app/src/main/java/com/example/android/simpleweather/utils/ConfigURL.java
+++ app/src/main/java/com/example/android/simpleweather/utils/ConfigURL.java
@@ -2,7 +2,7 @@ package com.example.android.simpleweather.utils;
 
 public class ConfigURL {
     final static String key =
-            "&key=5789543e92b113c9d506e3d8274f11a8";
+            "&key=649471936300090a4d76086198fa40a1";
     public final static String citiesURL =
             "http://v.juhe.cn/weather/citys?key=5789543e92b113c9d506e3d8274f11a8";
     private  static String weatherURL1 =
